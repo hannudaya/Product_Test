cdaad6208cd2eb7dab4506f97901bf96a4960bdb
hannudaya <hannudaya@gmail.com>
2023-09-13 11:28:39 +0100

    Add project files.

---
+++ Product_Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Product_Test.Startup))]
+namespace Product_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
